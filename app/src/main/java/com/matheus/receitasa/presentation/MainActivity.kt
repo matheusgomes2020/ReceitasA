90e550c739d8ed086bfea30de8dec28651277f01
matheusgomes2020 <matheusgomez2013@gmail.com>
2024-01-10 17:32:08 -0300

    navigation

--- app/src/main/java/com/matheus/receitasa/presentation/MainActivity.kt
+++ app/src/main/java/com/matheus/receitasa/presentation/MainActivity.kt
@@ -2,11 +2,31 @@ package com.matheus.receitasa.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.matheus.receitasa.R
+import com.matheus.receitasa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView( binding.root )
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNavMain.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
+
     }
 }
